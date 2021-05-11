8c61354941cb66028f3b02b23437ef7878298cb8
himaaaintake40 <mohamedebrahimabbas@gmail.com>
2021-05-11 14:47:00 +0200

    try CI

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\TestingController;
 use Illuminate\Support\Facades\Route;
 
 /*
